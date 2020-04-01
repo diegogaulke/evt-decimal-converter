540b910f39dcc050938ec7b7277573dae521c406
Diego Gaulke <diegobgaulke@gmail.com>
2020-04-01 12:26:27 -0300

    First commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Task group by day and hour to decimal converter
+
+This project reads a CSV file and groups the lines by date and sums the hours converting it to decimal.
+The format of the CSV file is the same as ```apontmes.csv```.
+
+To run:
+```python3 converter.py <file_name>```
---
+++ converter.py
@@ -0,0 +1,30 @@
+import csv
+import sys
+
+if len(sys.argv) != 2:
+    print('Please provide the csv file in the command line.')
+    exit()
+
+with open('apontmes.csv') as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=',')
+    line_count = 0
+
+    day_label_tmp = ''
+    day_total_tmp = 0
+
+    for row in csv_reader:
+        if line_count == 0:
+            line_count += 1
+            continue
+
+        if day_label_tmp == '' or day_label_tmp == row[0]:
+            day_label_tmp = row[0]
+        else:
+            print('%s - %s' % (day_label_tmp, day_total_tmp))
+            day_label_tmp = row[0]
+            day_total_tmp = 0
+
+        time_splitted = row[6].split(':')
+        day_total_tmp += round(float(time_splitted[0]) + (float(time_splitted[1]) / 60), 2)
+
+        line_count += 1
